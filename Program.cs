bbb255d42ebdb7a1788038c8a70b2267eb6cf131
devZahid <officialzahidhasan19932021@gmail.com>
2024-02-23 16:43:53 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Bank
+{
+    static void Main()
+    {
+        string userName = null,
+            passWord = null;
+        Console.WriteLine("***************Zahid Bank***************");
+        Console.WriteLine("::LOGIN PAGE::");
+
+        // taking input from user
+        Console.Write("User Name : ");
+        userName = Console.ReadLine();
+
+        if (userName != "")
+        {
+            Console.Write("Password : ");
+            passWord = Console.ReadLine();
+        }
+
+        if (userName == "zahid" && passWord == "123456")
+        {
+            Console.WriteLine("Successfully Login!!");
+        }
+        else
+        {
+            Console.WriteLine("Wrond username or password");
+        }
+
+        Console.ReadKey();
+        Console.WriteLine("Thank You!!!");
+        Console.ReadKey();
+    }
+}
